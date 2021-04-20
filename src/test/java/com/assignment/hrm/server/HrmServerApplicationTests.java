bcbd84f156d4d5eb430192f87945f5b1406cd044
Mohammad Noman <mohammad.noman@live.in>
2021-04-20 13:53:26 +0530

    fail commit

---
+++ src/test/java/com/assignment/hrm/server/HrmServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.assignment.hrm.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+class HrmServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertFalse(true);
+    }
+
+}
